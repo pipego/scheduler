701b096dc46a37946d86ccf36535d3a5c3d34e8b
Jia <angersax@sina.com>
2022-06-02 10:03:53 +0800

    fix(plugin): fix build error on plugin

--- script/plugin.sh
+++ script/plugin.sh
@@ -2,6 +2,10 @@
 
 list="plugin-fetch,plugin-filter,plugin-score"
 
+if [ ! -d bin ]; then
+  mkdir bin
+fi
+
 old=$IFS IFS=$','
 for item in $list; do
   URL=$(curl -L -s https://api.github.com/repos/pipego/$item/releases/latest | grep -o -E "https://(.*)${item}_(.*)_linux_amd64.tar.gz")
